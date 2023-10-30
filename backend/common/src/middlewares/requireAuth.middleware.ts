8d9c968245b9a3012afb14c20e9582e0e13dce83
ErezMizrahi <erezm@webtech-inv.co.il>
2023-10-30 17:06:12 +0200

    Update

--- backend/common/src/middlewares/requireAuth.middleware.ts
+++ backend/common/src/middlewares/requireAuth.middleware.ts
@@ -2,16 +2,8 @@ import { Request, Response, NextFunction } from "express";
 import { NotAuthorizedError } from "../errors/notAuthorized.error";
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
-    console.log('erez')
-    console.log(req.currentUser)
     if(!req.currentUser) {
-    console.log('erez2')
-
         throw new NotAuthorizedError();
     }
-
-    console.log('2222')
-
-
     next();
 }
