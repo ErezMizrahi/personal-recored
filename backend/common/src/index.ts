e6a5b6f7ca34f9d306c8ca41e75d71cb0845f164
ErezMizrahi <erezm@webtech-inv.co.il>
2024-03-20 13:14:26 +0200

    Update

--- backend/common/src/index.ts
+++ backend/common/src/index.ts
@@ -6,7 +6,7 @@ export * from './errors/notAuthorized.error';
 export * from './errors/notFound.error';
 export * from './errors/requestValidation.error';
 
-export * from './middlewares/currentUser.middleware';
+export * from './middlewares/currentGoogleUser.middleware';
 export * from './middlewares/error.middleware';
 export * from './middlewares/requireAuth.middleware';
 export * from './middlewares/validateRequest.middleware';
