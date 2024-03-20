553be40e6866d5c61d086b0f1f41ea530423f699
ErezMizrahi <erezm@webtech-inv.co.il>
2024-03-20 13:14:03 +0200

    Update

--- backend/common/src/middlewares/requireAuth.middleware.ts
+++ backend/common/src/middlewares/requireAuth.middleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { NotAuthorizedError } from "../errors/notAuthorized.error";
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
-    if(!req.currentUser) {
+    if(!req.currentGoogleUser) {
         throw new NotAuthorizedError();
     }
     next();
